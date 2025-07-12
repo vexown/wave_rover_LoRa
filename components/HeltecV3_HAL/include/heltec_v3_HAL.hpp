b3d7a36c2f91aaaa997a29b9f55d65cb0833b27d
blankMCU <vexown@gmail.com>
2025-07-12 23:40:13 +0200

    Add Heltec V3 HAL component

---
+++ components/HeltecV3_HAL/include/heltec_v3_HAL.hpp
@@ -0,0 +1,15 @@
+#ifndef HELTECV3_HAL_H
+#define HELTECV3_HAL_H
+
+/* Heltec V3 is developlment board with SX1262 LoRa module and a ESP32S3 MCU: https://heltec.org/project/wifi-lora-32-v3/
+ * This HAL provides an interface to interact with the board's hardware without worrying about the underlying implementation details.
+ *
+ * Datasheet:        https://resource.heltec.cn/download/WiFi_LoRa_32_V3/HTIT-WB32LA_V3.2.pdf
+ * Schematic (v3.1): https://resource.heltec.cn/download/WiFi_LoRa_32_V3/HTIT-WB32LA(F)_V3.1_Schematic_Diagram.pdf
+ * User manual:      https://docs.heltec.org/en/node/esp32/wifi_lora_32/index.html
+ * More resources:   https://resource.heltec.cn/download/WiFi_LoRa_32_V3/
+ */
+
+
+
+#endif // HELTECV3_HAL_H
